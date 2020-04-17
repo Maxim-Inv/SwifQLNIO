666f15238bc3fc3afb0e5bf52604e21cd848256d
Maxim Anisimov <maxim@invoodoo.com>
2020-04-17 22:25:24 +0300

    Add 10.15 macOS platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwifQLNIO",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(name: "SwifQLNIO", targets: ["SwifQLNIO"]),
         ],
